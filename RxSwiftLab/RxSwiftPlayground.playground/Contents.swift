57c6c579897aa611dca37c47dcee72af7950ad62
Reyes Sanchez Daniel (RBMX/BUD1-IO2) <Daniel.ReyesSanchez@mx.bosch.com>
2019-05-25 11:00:25 -0500

    Subjects added

--- RxSwiftLab/RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftLab/RxSwiftPlayground.playground/Contents.swift
@@ -30,29 +30,29 @@ import RxSwift
 //        })
 //}
 
-example(of: "4. Never") {
-    let observable = Observable<Any>.never()
-
-    observable
-        // Do Operator
-        .do(
-          onSubscribe: {
-            print("About to subscribe")
-        },
-          onDispose: {
-            print("Disposed")
-        })
-        // Regular Subscribe
-        .subscribe(onNext: { (element) in
-            print(element)
-        }, onError: { (error) in
-            print(error)
-        }, onCompleted: {
-            print("1. Completed")
-        }, onDisposed: {
-            print("2. Disposed")
-        })
-}
+//example(of: "4. Never") {
+//    let observable = Observable<Any>.never()
+//
+//    observable
+//        // Do Operator
+//        .do(
+//          onSubscribe: {
+//            print("About to subscribe")
+//        },
+//          onDispose: {
+//            print("Disposed")
+//        })
+//        // Regular Subscribe
+//        .subscribe(onNext: { (element) in
+//            print(element)
+//        }, onError: { (error) in
+//            print(error)
+//        }, onCompleted: {
+//            print("1. Completed")
+//        }, onDisposed: {
+//            print("2. Disposed")
+//        })
+//}
 
 
 //example(of: "4. Dispose") {
@@ -154,3 +154,112 @@ example(of: "4. Never") {
 //}
 
 
+/*
+ Subjects:
+ 
+    PublishSubject:
+        Starts empty
+        Emits onNext to new subscribers
+ 
+    BehaivorSubjet:
+        Starts with initial value
+        Replays initial / latest value to new subscribers
+ 
+ 
+    ReplaySubject:
+        Starts empty but with a buffer size
+        Replays buffered elements to new subscribers
+ 
+    Variable:
+        It's wrapped into a BehaviorSubject
+        Starts with initial value
+        replays initial / latest value to new subscribers
+        automatically completes
+        stores the latest value
+ 
+ */
+
+
+//example(of: "8. PublishSubject") {
+//    let quotes = PublishSubject<String>()
+//    quotes.onNext(itsNotMyFault)
+//
+//    let subscriptionOne = quotes
+//        .subscribe {
+//            print(label: "1)", event: $0)
+//        }
+//
+//    quotes.on(.next(doOrDoNot))
+//
+//    let subscriptionTwo = quotes
+//        .subscribe {
+//            print(label: "2)", event: $0)
+//        }
+//    quotes.onNext(lackOfFaith)
+//
+//    subscriptionOne.dispose()
+//
+//
+//    quotes.onNext(eyesCanDeceive)
+//    quotes.onCompleted()
+//
+//
+//    let subscriptionThree = quotes.subscribe {
+//        print(label: "2)", event: $0)
+//    }
+//
+//    quotes.onNext(stayOnTarget)
+//
+//    subscriptionThree.dispose()
+//    subscriptionTwo.dispose()
+//}
+
+//example(of: "9. BehaiviorSubject") {
+//    let bag = DisposeBag()
+//
+//    let quotes = BehaviorSubject.init(value: iAmYourFather)
+//
+//    let _ = quotes.subscribe {
+//        print(label: "1)", event: $0)
+//    }
+//
+//    quotes.onError(Quote.neverSaidThat)
+//    // it will emit the latest value: the onError value
+//    // subscription1 does not listen anymore
+//
+//    quotes.subscribe {
+//        // onError value also printed here
+//        print(label: "2)", event: $0)
+//    }.disposed(by: bag)
+//
+//}
+
+//example(of: "10. ReplaySubject") {
+//    let bag = DisposeBag()
+//    let subject = ReplaySubject<String>.create(bufferSize: 2)
+//
+//    subject.onNext(useTheForce)
+//    subject.subscribe {
+//        print(label: "1)", event: $0)
+//    }.disposed(by: bag)
+//
+//    subject.onNext(theForceIsStrong)
+//
+//    // this subscriber will receive both events emited before it's creation due to be a replaysubject
+//    subject.subscribe {
+//        print(label: "2)", event: $0)
+//    }
+//}
+
+example(of: "11. Variable") {
+    let bag = DisposeBag()
+    
+    let variable = Variable(mayTheForceBeWithYou)
+    print(variable.value)
+    
+    variable.asObservable().subscribe {
+        print(label: "1)", event: $0)
+    }.disposed(by: bag)
+    
+    variable.value = mayThe4thBeWithYou
+}
